a3dd4421c4e121397828d20499f436c5e2dfec64
Oware Ebenezer <nanaprestige71@gmail.com>
2026-05-16 19:09:29 -0700

    feat: add templates with base file, [style: add bootstrap framework to a static folder]

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/home")
-def home():
-    name = "Ella Smith."
-    return render_template("home.html",name = name)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
